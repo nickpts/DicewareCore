645bbe5f62a37f2fc6b72258d8cfdd32d7d8edf2
Nick <nicholas.patsaris@gmail.com>
2019-09-08 21:14:51 +0100

    Test changes

--- test/DicewareCore.Tests/DicewareTests.cs
+++ test/DicewareCore.Tests/DicewareTests.cs
@@ -24,24 +24,17 @@ namespace Tests
 		}
 
 		[Test]
-		public void Diceware_Generated_Throws_Argument_Exception_If_Number_Of_Words_Zero()
-		{
+		public void Diceware_Generated_Throws_Argument_Exception_If_Number_Of_Words_Zero() =>
 			Assert.Throws<ArgumentException>(() => generator.Create(0));
-		}
 
 		[Test]
-		public void Diceware_Generated_Throws_Argument_Exception_If_Number_Of_Words_Less_Than_Zero()
-		{
+		public void Diceware_Generated_Throws_Argument_Exception_If_Number_Of_Words_Less_Than_Zero() =>
 			Assert.Throws<ArgumentException>(() => generator.Create(-1));
-		}
-
 
 		[Test]
-		public void Diceware_Generated_Throws_Argument_Exception_If_Number_Of_Words_More_Than_Twenty()
-		{
+		public void Diceware_Generated_Throws_Argument_Exception_If_Number_Of_Words_More_Than_Twenty() =>
 			Assert.Throws<ArgumentException>(() => generator.Create(21));
-		}
-
+		
 		[Test]
 		public void Diceware_Create_Generates_Correct_Passphrase()
 		{
@@ -49,13 +42,12 @@ namespace Tests
             {
                 for (int i = 0; i < 100; i++)
                 {
-                    var pass = generator.Create(10, Language.Estonian, '-');
+                    var pass = generator.Create(10, Language.Basque, '-');
                     Debug.WriteLine(pass);
                 }
             }
 
             //TODO: chinese is weird
-            //TODO : czech dictionary missing
 		}
 	}
 }
